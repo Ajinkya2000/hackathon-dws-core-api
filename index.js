6e4829ffd6088bbba7b6644ea9a905e7ae782f43
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2024-09-10 15:22:36 +0530

    add dummy route

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //age income risk tolerance year of investment experience
 const express = require('express');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.send("Welcome to Tehalka Omlette!")
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on PORT:", PORT);
 })
